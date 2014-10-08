95652b87341a8cacf43adb5ef99776422c220dbb
Markus Török <markus.toeroek@gmail.com>
2014-10-08 22:14:49 +0200

    added validation and shoulda_matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Category do
+  before(:each) do
+    @c = Category.new(title: "Drama")
+  end
 
+  it "saves itself" do
+    @c.save
+    expect(Category.first).to eq(@c)
+  end
+
+  it "should have many videos" do
+    c = Category.reflect_on_association(:videos)
+    expect(c.macro).to eq(:has_many)
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,9 +1,26 @@
 require 'spec_helper'
+require 'shoulda/matchers'
 
 describe Video do
+  before(:each) do
+    @video = Video.new(title: "Monk", description: "a great video!")
+  end
+
+
   it "saves itself" do
-    video = Video.new(title: "Monk", description: "a great video!")
-    video.save
-    expect(Video.first).to eq(video)
+    @video.save
+    expect(Video.first).to eq(@video)
+  end
+
+  it { should belong_to(:category)}
+
+  it "should require a title" do
+    v = Video.new(description: "a great video")
+    expect(v).not_to be_valid
+  end
+
+  it "should require a description" do
+    v = Video.new(title: "South Park")
+    expect(v).not_to be_valid
   end
 end
