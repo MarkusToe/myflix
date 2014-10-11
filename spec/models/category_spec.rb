757f1b30df58af6884e65500d2f1c9817daa1f86
Markus Török <markus.toeroek@gmail.com>
2014-10-11 20:26:34 +0200

    implemented shoulda matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,17 +1,10 @@
 require 'spec_helper'
+require 'shoulda/matchers'
 
 describe Category do
   before(:each) do
     @c = Category.new(title: "Drama")
   end
 
-  it "saves itself" do
-    @c.save
-    expect(Category.first).to eq(@c)
-  end
-
-  it "should have many videos" do
-    c = Category.reflect_on_association(:videos)
-    expect(c.macro).to eq(:has_many)
-  end
+  it { should have_many(:videos) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,21 +6,7 @@ describe Video do
     @video = Video.new(title: "Monk", description: "a great video!")
   end
 
-
-  it "saves itself" do
-    @video.save
-    expect(Video.first).to eq(@video)
-  end
-
-  it { should belong_to(:category)}
-
-  it "should require a title" do
-    v = Video.new(description: "a great video")
-    expect(v).not_to be_valid
-  end
-
-  it "should require a description" do
-    v = Video.new(title: "South Park")
-    expect(v).not_to be_valid
-  end
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 end
