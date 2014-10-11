8012e19e346f1eaa72cf8b9ff125e896be445014
Markus Török <markus.toeroek@gmail.com>
2014-10-11 21:17:23 +0200

    remove test objects

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,9 +2,5 @@ require 'spec_helper'
 require 'shoulda/matchers'
 
 describe Category do
-  before(:each) do
-    @c = Category.new(title: "Drama")
-  end
-
   it { should have_many(:videos) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 require 'shoulda/matchers'
 
 describe Video do
-  before(:each) do
-    @video = Video.new(title: "Monk", description: "a great video!")
-  end
-
   it { should belong_to(:category) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
